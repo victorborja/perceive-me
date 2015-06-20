544d1597d5cad06bbf2da833f74e26f6c1551a42
Victor Borja <vborja@apache.org>
2015-06-20 18:56:44 -0500

    Upload and show images

--- collections/people.js
+++ collections/people.js
@@ -1,20 +1,44 @@
 
 People = new Meteor.Collection('people');
 
+Images = new FS.Collection("images", {
+  stores: [new FS.Store.FileSystem("images", {})]
+});
+
+Images.allow({
+  insert: function (userId, doc) {
+    return true;
+  },
+  download: function (userId) {
+    return true;
+  }
+});
 
 People.attachSchema(new SimpleSchema({
 
   story_a: {
     label: 'Pregunta 1',
-    type: 'String'
+    type: String
   },
   story_b: {
     label: 'Pregunta 2',
-    type: 'String'
+    type: String
   },
   story_c: {
     label: 'Pregunta 3',
-    type: 'String'
+    type: String
+  },
+
+  picture: {
+    type: String,
+    autoform: {
+      afFieldInput: {
+        type: 'fileUpload',
+        collection: 'Images',
+        label: 'A picture of you' // optional
+      }
+    }
   }
 
-}))
+}));
+
